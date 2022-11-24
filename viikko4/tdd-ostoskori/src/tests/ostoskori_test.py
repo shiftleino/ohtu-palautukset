2eccc8d1f2fed138f2a6fe2e8d9cd25d072e619f
shiftleino <shiftleino@gmail.com>
2022-11-24 10:27:42 +0200

    Return purchase with item name and count 2 if two same items added

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -73,3 +73,13 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(maito)
         ostokset = self.kori.ostokset()
         self.assertEqual(len(ostokset), 1)
+    
+    def test_kahden_saman_tuotteen_lisaamisen_jalkeen_korissa_ostos_jolla_oikeat_tiedot(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(maito)
+        ostos = self.kori.ostokset()[0]
+        self.assertEqual(ostos.tuotteen_nimi(), "Maito")
+        self.assertEqual(ostos.lukumaara(), 2)
+
+        
