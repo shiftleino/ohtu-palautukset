9daddd4f9b7c39639df7f51baa70c6d50510c532
shiftleino <shiftleino@gmail.com>
2022-11-24 09:58:57 +0200

    Return two items if two items added

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -19,3 +19,10 @@ class TestOstoskori(unittest.TestCase):
         maito = Tuote("Maito", 3)
         self.kori.lisaa_tuote(maito)
         self.assertEqual(self.kori.hinta(), 3)
+
+    def test_kahden_eri_tuotteen_lisaamisen_jalkeen_korissa_kaksi_tavaraa(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        kahvi = Tuote("Kahvi", 7)
+        self.kori.lisaa_tuote(kahvi)
+        self.assertEqual(self.kori.tavaroita_korissa(), 2)
