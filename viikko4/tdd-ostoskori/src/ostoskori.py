5b7f34ce4358aebc2fafc10e58b8982d705f1071
shiftleino <shiftleino@gmail.com>
2022-11-24 11:13:23 +0200

    Week4 Exercise5

--- viikko4/tdd-ostoskori/src/ostoskori.py
+++ viikko4/tdd-ostoskori/src/ostoskori.py
@@ -3,13 +3,9 @@ from ostos import Ostos
 
 class Ostoskori:
     def __init__(self):
-        # ostoskori tallettaa Ostos-oliota, yhden per korissa oleva Tuote
         self._sisalto = []
 
     def tavaroita_korissa(self):
-        # kertoo korissa olevien tavaroiden lukumäärän
-        # eli jos koriin lisätty 2 kpl tuotetta "maito", tulee metodin palauttaa 2 
-        # samoin jos korissa on 1 kpl tuotetta "maito" ja 1 kpl tuotetta "juusto", tulee metodin palauttaa 2 
         return sum([ostos.lukumaara() for ostos in self._sisalto])
 
     def hinta(self):
@@ -36,6 +32,4 @@ class Ostoskori:
         self._sisalto = []
 
     def ostokset(self):
-        # palauttaa listan jossa on korissa olevat ostos-oliot
-        # kukin ostos-olio siis kertoo mistä tuotteesta on kyse JA kuinka monta kappaletta kyseistä tuotetta korissa on
         return self._sisalto
