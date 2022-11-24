0970297cf1c8ba2211755db4dc5bb817f224d2f4
shiftleino <shiftleino@gmail.com>
2022-11-24 10:13:10 +0200

    Return two purchases if two different items added

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -58,3 +58,11 @@ class TestOstoskori(unittest.TestCase):
         ostos = self.kori.ostokset()[0]
         self.assertEqual(ostos.tuotteen_nimi(), "Maito")
         self.assertEqual(ostos.lukumaara(), 1)
+
+    def test_kahden_eri_tuotteen_lisaamisen_jalkeen_korissa_kaksi_ostosta(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        kahvi = Tuote("Kahvi", 7)
+        self.kori.lisaa_tuote(kahvi)
+        ostokset = self.kori.ostokset()
+        self.assertEqual(len(ostokset), 2)
