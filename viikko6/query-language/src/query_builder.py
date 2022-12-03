8c3f9ffdfd2a24cdb5515124cc4b061cab7a8497
shiftleino <shiftleino@gmail.com>
2022-12-03 18:34:19 +0200

    Week6 Exercise4

---
+++ viikko6/query-language/src/query_builder.py
@@ -0,0 +1,18 @@
+from matchers import And, HasAtLeast, PlaysIn, Not, All, HasFewerThan, Or
+
+
+class QueryBuilder:
+    def __init__(self, matcher=All()):
+        self._matcher_obj = matcher
+
+    def playsIn(self, team):
+        return QueryBuilder(And(PlaysIn(team), self._matcher_obj))
+    
+    def hasAtLeast(self, value, attr):
+        return QueryBuilder(And(HasAtLeast(value, attr), self._matcher_obj))
+    
+    def hasFewerThan(self, value, attr):
+        return QueryBuilder(And(HasFewerThan(value, attr), self._matcher_obj))
+
+    def build(self):
+        return self._matcher_obj
